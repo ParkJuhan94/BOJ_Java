b59ccd16ecff9cc6fbe607bfc9b2e591ba00934a
JuHan <1004sunye@naver.com>
2022-08-31 07:06:33 +0900

    start

---
+++ src/BOJ/Silver/P10773/Main.java
@@ -0,0 +1,22 @@
+package BOJ.Silver.P10773;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/BOJ/Silver/P/input.txt"));
+        System.setIn(new FileInputStream("src/BOJ/Gold/P/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+
+}
